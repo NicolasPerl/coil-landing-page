203a3f62c041f386fcc2cc752ed1a30cf086fc2f
Nicolas Perlick <nicolasperlick@Nicolass-iMac.local>
2017-09-09 01:04:52 -0500

    analytics integrated

--- index.php
+++ index.php
@@ -18,7 +18,15 @@
     <link rel="shortcut icon" type="image/x-icon" href="images/favicon.ico" />
     <!--google fonts-->
     <link href="https://fonts.googleapis.com/css?family=Lato:black,400,900" rel="stylesheet">
+    <script>
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
+        ga('create', 'UA-90907253-2', 'auto');
+        ga('send', 'pageview');
+    </script>
   </head>
   <body>
       <div class="col-sm-12">
--- product.php
+++ product.php
@@ -25,6 +25,18 @@
     <!-- Lato font -->
     <link href="https://fonts.googleapis.com/css?family=Lato:black,light,400,900" rel="stylesheet">
     <link rel="shortcut icon" type="image/x-icon" href="images/favicon.ico" />
+
+    <!--Google analytics-->
+    <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+      ga('create', 'UA-90907253-2', 'auto');
+      ga('send', 'pageview');
+
+    </script>
   </head>
   <body>
         <div class="row row1">
